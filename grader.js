d3bff83d5a1ace39d673166454647314c976ae8a
warlck <addick@me.com>
2013-07-18 02:05:04 +0800

    Add restler and extra command line option to be able to grade pages from the web. When url is given as --url option grader gets the contents of page and checks for elements

--- grader.js
+++ grader.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -40,14 +41,48 @@ var clone = function(fn) {
 	return fn.bind({});
 };
 
+var printResult = function(checkJson) {
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+}
+
+var cheerioHtml = function (string) {
+	return cheerio.load(string);
+}
+
+var checkHtml = function(html, checksfile) {
+	$ = cheerioHtml(html);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for (var li in checks) {
+		var present = $(checks[li]).length > 0;
+		out[checks[li]] = present;
+	}
+	return out;
+};
+
 if(require.main == module ) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL of the page to be checked')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
-} else {
-	exports.checkHtmlFile = checkHtmlFile;
-}
+
+		if (program.url) {
+			 rest.get(program.url).on('complete', function(result) {
+			  if (result instanceof Error) {
+			    sys.puts('Error: ' + result.message);
+			    this.retry(5000); // try again after 5 sec
+			  } else {
+			  	var checkJson = checkHtml(result.toString(), program.checks);
+				printResult(checkJson);
+			  }
+			});
+		}
+		else if (program.file)  {	
+			var checkJson = checkHtmlFile(program.file, program.checks);
+			printResult(checkJson);
+		}
+	} else {
+		exports.checkHtmlFile = checkHtmlFile;
+	}
